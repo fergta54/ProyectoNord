8c836e0a4d4d2984fb30f9a8f7c318118d2c20a9
MAURIGON <102996854+MAURIGON@users.noreply.github.com>
2023-06-06 03:41:30 -0400

    33-comp-marcas

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
 
-$servidor = 'localhost';
-$usu = 'root';
-$password = '';
-$bd = 'nordsternazul';
+// $servidor = 'localhost';
+// $usu = 'root';
+// $password = '';
+// $bd = 'nordsternazul';
 //---------------------------------------
-// $servidor='MYSQL8001.site4now.net';
-// $usu='a9a738_nrdazul';
-// $password='GrupoAzul777';
-// $bd='db_a9a738_nrdazul';
+$servidor = 'MYSQL8001.site4now.net';
+$usu = 'a9a738_nrdazul';
+$password = 'GrupoAzul777';
+$bd = 'db_a9a738_nrdazul';
 
 $conexion = mysqli_connect($servidor, $usu, $password, $bd);
 
