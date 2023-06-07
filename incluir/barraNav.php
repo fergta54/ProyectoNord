61e1f9862a3d40be2f71e136278755a6a55d0405
mauri6559 <mauriciogarciam22@gmail.com>
2023-06-07 14:10:14 -0400

    Update barraNav.php

--- incluir/barraNav.php
+++ incluir/barraNav.php
@@ -64,7 +64,8 @@ if ($varSession == null || $varSession = '' || $varRol != 'admin') {
                 </li>
                 <li class="nav-item pl-4">
                     <a class="cabecera" href="../admin.php">
-                        Gestión de Comprasholaaaa
+                        Gestión de Comprasholaaaasssss
+                        hola mundo
                     </a>
                 </li>
                 <li class="nav-item pl-4">
