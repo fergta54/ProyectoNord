1277c2d7a2979118a30d9e9259238e096bfd01db
LuciaBeVI <alymaro96@gmail.com>
2023-06-07 18:10:25 -0400

    Update navVentas.php

--- gestionVentas/navVentas.php
+++ gestionVentas/navVentas.php
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container px-4 px-lg-5">
             <a class="cabecera" href="index.php">
-                <img src="./recursos/img/lg.png" alt="logo" />
+                <img src="../recursos/img/lg.png" alt="logo" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
