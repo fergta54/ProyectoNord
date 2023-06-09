901726daea99f0982f66a4578ba1fb650b24cc7a
MAURIGON <102996854+MAURIGON@users.noreply.github.com>
2023-06-09 13:53:12 -0400

    olap1

---
+++ conexionOlap.php
@@ -0,0 +1,17 @@
+<?php
+
+// $servidor = 'localhost';
+// $usu = 'root';
+// $password = '';
+// $bd = 'nordsternazul';
+// ---------------------------------------
+$servidor = 'MYSQL8001.site4now.net';
+$usu = 'a9a738_nrdazul';
+$password = 'GrupoAzul777';
+$bd = 'db_a9a738_nrdazul';
+
+$conexion = mysqli_connect($servidor, $usu, $password, $bd);
+
+// $conexion = mysqli_connect('localhost', 'root', '', 'nordsternazul');
+
+
